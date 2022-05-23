5054683e97188e8d0d06718a508128f6672a5cd8
MarisolM09 <mmendez9980@gmail.com>
2022-05-23 16:31:09 -0500

    added button with number and email

--- main.js
+++ main.js
@@ -9,21 +9,31 @@ console.log(listNames)
         let fetchedData = data.results;
         console.log(fetchedData)
 
+        // button function
+
+
         fetchedData.forEach(element => {
             const li = document.createElement("li");
             const h2 = document.createElement("h2");
             const img = document.createElement("img");
             const btn = document.createElement("button");
+            const div =  document.createElement("div");
             
-            h2.innerText = Object.values(element.name);
+            h2.innerText = (element.name.first+ " "+element.name.last);
             img.src = element.picture.thumbnail;
             btn.innerText = "More Details";
+            div.innerText = (element.email+ " "+element.phone);
            
             li.appendChild(h2);
             li.appendChild(img);
             li.appendChild(btn);
             listNames.appendChild(li);
-           
+            
+            btn.addEventListener('click', function() {
+                li.appendChild(div);
+
+
+            });
             
         })
     });
